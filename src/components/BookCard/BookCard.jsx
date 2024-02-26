75ccd00b45d659743ceb4a0ecc718f7a470c0059
raihanuldev <rihanthedev@gmail.com>
2024-02-26 23:30:57 +0600

    Top books Data added

---
+++ src/components/BookCard/BookCard.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const BookCard = ({book}) => {
+    return (
+        <div className="card w-96 bg-base-100 shadow-xl">
+            <figure><img src={book.bookImg} alt="book" /></figure>
+            <div className="card-body">
+                <h2 className="card-title">
+                    {book.bookName}!
+                    <div className="badge badge-secondary">Best Sell</div>
+                </h2>
+                <p>{book.bookDescription}</p>
+                <div className="card-actions justify-end">
+                    <div className="badge badge-outline">Motivational</div>
+                    <div className="badge badge-outline">Self</div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default BookCard;
--- src/components/topBook/TopBook.jsx
+++ src/components/topBook/TopBook.jsx
@@ -1,50 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import BookCard from '../BookCard/BookCard';
 
 const TopBook = () => {
+    const [TopBok,setTopBook] = useState([]);
+    useEffect(()=>{
+        fetch('/books.json').then(res=>res.json()).then(data=>setTopBook(data))
+    },[])
     return (
         <div className='flex space-x-4 justify-center'>
-            <div className="card w-96 bg-base-100 shadow-xl">
-                <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-                <div className="card-body">
-                    <h2 className="card-title">
-                        Shoes!
-                        <div className="badge badge-secondary">NEW</div>
-                    </h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                        <div className="badge badge-outline">Fashion</div>
-                        <div className="badge badge-outline">Products</div>
-                    </div>
-                </div>
-            </div>
-            <div className="card w-96 bg-base-100 shadow-xl">
-                <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-                <div className="card-body">
-                    <h2 className="card-title">
-                        Shoes!
-                        <div className="badge badge-secondary">NEW</div>
-                    </h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                        <div className="badge badge-outline">Fashion</div>
-                        <div className="badge badge-outline">Products</div>
-                    </div>
-                </div>
-            </div>
-            <div className="card w-96 bg-base-100 shadow-xl">
-                <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-                <div className="card-body">
-                    <h2 className="card-title">
-                        Shoes!
-                        <div className="badge badge-secondary">NEW</div>
-                    </h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                        <div className="badge badge-outline">Fashion</div>
-                        <div className="badge badge-outline">Products</div>
-                    </div>
-                </div>
-            </div>
+            {
+                TopBok.slice(0,3).map(book=> <BookCard key={book.id} book = {book}></BookCard>)
+            }     
         </div>
 
     );
