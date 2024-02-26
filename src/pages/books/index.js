d7bb3d460499fdfe87f36dca702ca487bbcab983
raihanuldev <rihanthedev@gmail.com>
2024-02-27 00:08:59 +0600

    books route

--- src/pages/books/index.js
+++ src/pages/books/index.js
@@ -15,7 +15,7 @@ const BooksPage = () => {
                 <input type="text" placeholder="Type Book Name.." className="input input-bordered w-full max-w-xs" />
                 <button className='btn btn-primary ml-4'>Search</button>
             </div>
-            <div className='flex'>
+            <div className='grid grid-cols-1 md:grid-cols-3 p-12 py-3 mx-3 '>
                 {
                     allbooks.map(book => <BookCard key={book.id} book={book} />)
                 }
